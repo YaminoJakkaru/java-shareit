9d4c4f3a643354d37795c43fb0bc5163eb94193f
Ваня <undeadkivva@mail.ru>
2023-03-08 23:53:41 +0300

    style: исправлены стилистические ошибки

--- src/main/java/ru/practicum/shareit/item/exception/ItemNotFoundException.java
+++ src/main/java/ru/practicum/shareit/item/exception/ItemNotFoundException.java
@@ -1,6 +1,8 @@
 package ru.practicum.shareit.item.exception;
 
-public class ItemNotFoundException extends RuntimeException{
+public class ItemNotFoundException extends RuntimeException {
 
-    public ItemNotFoundException(){}
+    public ItemNotFoundException() {
+
+    }
 }
--- src/main/java/ru/practicum/shareit/user/exception/UserNotFoundException.java
+++ src/main/java/ru/practicum/shareit/user/exception/UserNotFoundException.java
@@ -1,5 +1,7 @@
 package ru.practicum.shareit.user.exception;
 
 public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException() {}
+    public UserNotFoundException() {
+
+    }
 }
